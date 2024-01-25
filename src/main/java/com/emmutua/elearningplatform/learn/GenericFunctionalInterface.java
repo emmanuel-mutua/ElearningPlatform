2707cf31a43bbdd6777587f5d0cc739f8683c265
emmanuel-mutua <emmamulwa2020@gmail.com>
2024-01-25 10:56:24 +0300

    generic fun interface and lambda with a parameter

---
+++ src/main/java/com/emmutua/elearningplatform/learn/GenericFunctionalInterface.java
@@ -0,0 +1,31 @@
+package com.emmutua.elearningplatform.learn;
+
+/**
+ * Generic Functional interfaces (They can allow any data type)
+ */
+@FunctionalInterface
+public interface GenericFunctionalInterface<T> {
+    T func(T arg);
+}
+
+
+/**
+ * Lambda is anonymous/unnamed method
+ * Now lambda expression does not execute on it-self but impl a method defined on a functional interface
+ */
+
+class MyClass{
+    public static void main(String[] args) {
+        //Get name
+        GenericFunctionalInterface<String> genericFunctionalInterface = (id) -> "Emmanuel Mulwa";
+        System.out.println(genericFunctionalInterface.func("dfjkafl"));
+
+        Double myDouble = 2.12;
+        GenericFunctionalInterface<Double>  genericFunctionalInterface1= (myDoubleArg) -> {
+            double v = myDoubleArg * myDoubleArg;
+            return v;
+        };
+
+        System.out.println(genericFunctionalInterface1.func(myDouble));
+    }
+}
